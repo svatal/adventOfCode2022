ee5776bf49c0ae5f670a93f4540bd99a1c94c5c4
svatal <swata@centrum.cz>
2022-12-15 22:51:30 +0100

    15 refactored

--- src/15.ts
+++ src/15.ts
@@ -1,7 +1,6 @@
 // import { testInput as input } from "./15-input";
 import { input } from "./15-input";
-import { logEvery } from "./utils/log";
-import { IPosition, posToString } from "./utils/position2D";
+import { IPosition } from "./utils/position2D";
 
 export function doIt() {
   const parsed = input
@@ -13,30 +12,30 @@ export function doIt() {
         .map(([x, y]) => ({ x, y }))
     )
     .map(([sensor, beacon]) => ({ sensor, beacon }));
+  let startTime = Date.now();
   const examineY = 2000000;
   //   const examineY = 10;
   const f = new Set<number>();
-  //   const occupied = new Set(
-  //     [...parsed.map((p) => p.sensor), ...parsed.map((p) => p.beacon)]
-  //       .filter((p) => p.y === examineY)
-  //       .map((p) => p.x)
-  //   );
-  //   function add(x: number) {
-  //     if (!occupied.has(x)) f.add(x);
-  //   }
-  //   //   console.log(parsed);
-  //   parsed.forEach(({ sensor, beacon }) => {
-  //     const safeDist = dist(sensor, beacon);
-  //     const rowDist = Math.abs(sensor.y - examineY);
-  //     for (let i = 0; i <= safeDist - rowDist; i++) {
-  //       add(sensor.x + i);
-  //       add(sensor.x - i);
-  //     }
-  //   });
-  const first = f.size;
-  let found = false;
-  for (let y = /*2500*/ 400000; y <= 4000000; y++) {
-    for (let x = 0; x <= 4000000; ) {
+  const occupied = new Set(
+    [...parsed.map((p) => p.sensor), ...parsed.map((p) => p.beacon)]
+      .filter((p) => p.y === examineY)
+      .map((p) => p.x)
+  );
+  function add(x: number) {
+    if (!occupied.has(x)) f.add(x);
+  }
+  parsed.forEach(({ sensor, beacon }) => {
+    const safeDist = dist(sensor, beacon);
+    const rowDist = Math.abs(sensor.y - examineY);
+    for (let i = 0; i <= safeDist - rowDist; i++) {
+      add(sensor.x + i);
+      add(sensor.x - i);
+    }
+  });
+  console.log("first", f.size, "time:", Date.now() - startTime);
+  startTime = Date.now();
+  for (let y = 0; y <= 2 * examineY; y++) {
+    for (let x = 0; x <= 2 * examineY; ) {
       const position = { x, y };
       const remainingDist = Math.max(
         ...parsed.map(
@@ -44,22 +43,18 @@ export function doIt() {
         )
       );
       if (remainingDist < 0) {
-        console.log(position);
+        console.log(
+          "second",
+          position,
+          position.x * 2 * examineY + position.y,
+          "time",
+          Date.now() - startTime
+        );
         return;
       }
       x += remainingDist + 1;
-      //   found = parsed.every(
-      //     ({ sensor, beacon }) => dist(sensor, beacon) < dist(sensor, position)
-      //   );
-      //   if (found) {
-      //     console.log(position);
-      //     return;
-      //   }
     }
-    if (y % 1000000 === 0) console.log(y);
   }
-  const second = parsed.length;
-  console.log(first, second);
 }
 
 function dist(p1: IPosition, p2: IPosition) {
