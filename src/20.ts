029087d9d8a4952b75f55ef7d317d9bd7ca5bbff
svatal <swata@centrum.cz>
2022-12-20 17:02:42 +0100

    20 cleaned up

--- src/20.ts
+++ src/20.ts
@@ -10,18 +10,23 @@ interface IContainer {
 
 export function doIt(progress: (...params: any[]) => void) {
   const parsed = input.split(`\n`).map((line, i, all) => ({
-    n: +line * 811589153,
+    n: +line,
     idx: i,
     nextIdx: (i + 1) % all.length,
     prevIdx: (i - 1 + all.length) % all.length,
   }));
-  for (let turn = 0; turn < 10; turn++) {
+  const first = mix(parsed, 1, 1);
+  const second = mix(parsed, 811589153, 10);
+  console.log(first, second);
+}
+
+function mix(parsed: IContainer[], decryptionKey: number, times: number) {
+  parsed = parsed.map((p) => ({ ...p, n: p.n * decryptionKey }));
+  for (let turn = 0; turn < times; turn++) {
     for (let ci = 0; ci < parsed.length; ci++) {
       const c = parsed[ci];
       let move = c.n % (parsed.length - 1);
       while (move < 0) move += parsed.length - 1;
-      // console.log(parsed);
-      // console.log("move", c, move);
       if (move === 0) continue;
       let target = c;
       for (let i = 0; i < move; i++) target = parsed[target.nextIdx];
@@ -33,14 +38,16 @@ export function doIt(progress: (...params: any[]) => void) {
       target.nextIdx = c.idx;
     }
   }
-  // console.log(parsed);
+  return getCoordinates(parsed);
+}
+
+function getCoordinates(parsed: IContainer[]) {
   const zeroC = parsed.find((c) => c.n === 0)!;
-  let first = 0;
+  let result = 0;
   let current = zeroC;
   for (let i = 0; i < 3001; i++) {
-    if (i === 1000 || i === 2000 || i === 3000) first += current.n;
+    if (i === 1000 || i === 2000 || i === 3000) result += current.n;
     current = parsed[current.nextIdx];
   }
-  const second = parsed.length;
-  console.log(first, second);
+  return result;
 }
